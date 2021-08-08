8c3ceab2eb125a767a5b0bb9937599ea32d7c3a6
Jan Baer <jan@janbaer.de>
2021-08-08 18:25:13 +0200

    Add support for testing with Jest

---
+++ src/services/search-query-builder.service.spec.js
@@ -0,0 +1,9 @@
+import SearchQueryBuilderService from './search-query-builder.service';
+
+describe('SearchQueryBuilderService spec', () => {
+  describe('When Builder should return something', () => {
+    it('Should be true', () => {
+      expect(true).toBe(true);
+    });
+  });
+});
