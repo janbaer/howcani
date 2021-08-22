bf9cc51de34981b5bede570303d91e0e3ff9d2ac
Jan Baer <jan@janbaer.de>
2021-08-22 20:11:40 +0200

    Configure preprocessing of styles for Svelte with Postcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess({
+        defaults: {
+          style: 'postcss'
+        },
+        postcss: true
+      })
+    })
+  ],
+  resolve: {
+    alias: {
+      '/@/': '/src/'
+    }
+  }
 });
