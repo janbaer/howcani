7a685e22c83d53e35be30cb23b39a67eb9a05033
Jan Baer <jan@janbaer.de>
2021-12-18 18:46:30 +0100

    Add support for deploying with GitHub actions

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "Please pass a version as command arg"
+  exit 1
+fi
+
+echo "Deploying new version $VERSION to gh-pages branch"
+
+mkdir -p ./deploy
+
+cd ./deploy
+
+git rm -r .
+
+cp -R ../build/. .
+
+rm *.map
+
+if [ -f "./styles/*.*.map" ]; then
+  rm ./styles/*.*.map
+fi
+
+touch .nojekyll
+
+echo -n "howcani.janbaer.de" > CNAME
+
+git add -u && git add .
+
+git commit -m "Version ${VERSION}"
+
+# if [ ! ${CI} = true ]; then
+  # git push -u origin gh-pages
+# fi
+
