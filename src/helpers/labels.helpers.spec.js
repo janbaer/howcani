2a226462f0cad34c899960a769b5beb623127eb3
Jan Baer <jan@janbaer.de>
2022-07-01 17:44:45 +0200

    Replace Jest with Vitest

--- src/helpers/labels.helpers.spec.js
+++ src/helpers/labels.helpers.spec.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { mapLabelNames } from './labels.helpers';
 
 describe('LabelsHelpers spec', () => {
--- src/services/search-query-builder.service.spec.js
+++ src/services/search-query-builder.service.spec.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 // import SearchQueryBuilderService from './search-query-builder.service';
 
 describe('SearchQueryBuilderService spec', () => {
