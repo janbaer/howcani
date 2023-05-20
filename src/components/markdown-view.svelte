61e997451e0a106e1a3bc327311a2b8f71585574
Jan Baer <jan@janbaer.de>
2023-05-20 17:42:45 +0200

    Update npm-packages

--- src/components/markdown-view.svelte
+++ src/components/markdown-view.svelte
@@ -3,8 +3,10 @@
   import 'github-markdown-css/github-markdown-light.css';
 
   export let content = '';
+
+  const markedOptions = { mangle: false, headerIds: false, headerPrefix: false };
 </script>
 
 <div class="markdown-body">
-  {@html marked(content)}
+  {@html marked(content, markedOptions)}
 </div>
